fb3c128c0b4250c1cc41e4b360a34bf11bee0d54
hamwid <hampus.widell@alleman.nu>
2018-12-17 10:36:59 +0100

    Updated according to Simon's feedback

--- IAD.java
+++ IAD.java
@@ -38,8 +38,8 @@ public class IAD {
 
   //Analyze data from connected IHD(s) and set output-data
   private void analyzeData(){
-    input1 = ihd1.getRead1();
-    input2 = ihd2.getRead2();
+    input1 = ihd1.getOutput();
+    input2 = ihd2.getOutput();
     output1 = (input1 > IHD_limit);
     output2 = (input2 > IHD_limit);
   }
